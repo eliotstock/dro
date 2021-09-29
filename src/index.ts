5f34f80cda8b1c0a3ba9401a81429a9355916fae
Eliot Stock <1@eliotstock.com>
2021-09-29 21:52:18 +1300

    Range width in ticks - arithmetic fixed.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { ethers } from "ethers";
-import { nearestUsableTick, Pool, Position, tickToPrice } from "@uniswap/v3-sdk";
-import { Token, CurrencyAmount, Percent } from "@uniswap/sdk-core";
+import { nearestUsableTick, Pool, Position, priceToClosestTick, tickToPrice } from "@uniswap/v3-sdk";
+import { Token, CurrencyAmount, Percent, Price, Fraction } from "@uniswap/sdk-core";
 import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
 
 // TODO
 // ----
-
-// (P1) Show the new range min and max in terms of USDC rather than ticks
 
 // (P2) Mint a new liquidity position (but fail because no local account) centred on the current price, providing half ETH and half USDC
 // (P2) Remove an existing liquidity position (but fail because no local account)
@@ -26,14 +24,13 @@ import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/i
 // Done
 // ----
 
+// (P1) Fix the range width arithmetic
+// (P1) Show the new range min and max in terms of USDC rather than ticks
 // (P1) Get the current price in the pool synchronously and in terms of the quote currency
 // (P1) Know when we're out of range, indirectly, based on the current price in the pool and the current min/max, which we'll store for now
 // (P2) Execute everything on every new block by subscribing to "block""
 // (P3) Understand whether executing on every block is going to spend the free quota at Infura
 // (P3) Switch to a local geth node if we're going to run out of Infura quota
-
-// Width of the range relative to the current price.
-const RANGE_WIDTH = 0.01;
 
 // My personal Infura project (dro). Free quota is 100K requests per day, which is more than one a second.
 // WSS doesn't work ("Error: could not detect network") and HTTPS works for event subscriptions anyway.
@@ -60,6 +57,7 @@ const poolContract = new ethers.Contract(
   PROVIDER
 );
 
+// Single, global instance of the DRO class.
 let dro: DRO;
 
 interface Immutables {
@@ -82,13 +80,37 @@ interface State {
   unlocked: boolean;
 }
 
-interface DRO {
-  poolImmutables: Immutables,
-  usdc: Token,
-  weth: Token,
-  priceUsdc: string,
-  minTick: number,
-  maxTick: number
+class DRO {
+  readonly poolImmutables: Immutables;
+  readonly usdc: Token;
+  readonly weth: Token;
+  priceUsdc: string = "unknown";
+  minTick: number = 0;
+  maxTick: number = 0;
+  rangeWidthTicks = 0;
+
+  constructor(_poolImmutables: Immutables, _usdc: Token, _weth: Token, _rangeWidthTicks: number) {
+    this.poolImmutables = _poolImmutables;
+    this.usdc = _usdc;
+    this.weth = _weth;
+    this.rangeWidthTicks = _rangeWidthTicks;
+  }
+
+  outOfRange(currentTick: number) {
+    return currentTick < this.minTick || currentTick > this.maxTick;
+  }
+
+  // Note that if rangeWidthTicks is not a multiple of the tick spacing for the pool, the range
+  // returned here can be quite different to rangeWidthTicks.
+  newRange(currentTick: number) {
+    const minTick = nearestUsableTick(Math.round(currentTick - (this.rangeWidthTicks / 2)),
+      this.poolImmutables.tickSpacing);
+
+    const maxTick = nearestUsableTick(Math.round(currentTick + (this.rangeWidthTicks / 2)),
+      this.poolImmutables.tickSpacing);
+
+    return [minTick, maxTick];
+  }
 }
 
 async function getPoolImmutables() {
@@ -140,32 +162,12 @@ async function getPoolState() {
   return PoolState;
 }
 
-function minMaxTicks(currentTick: number, rangeWidth: number, tickSpacing: number) {
-  console.log("rangeWidth / 2: ", rangeWidth / 2);
-  console.log("(rangeWidth / 2) * currentTick: ", (rangeWidth / 2) * currentTick);
-  console.log("minTick: ", currentTick - ((rangeWidth / 2) * currentTick));
-
-  let minTick = currentTick - ((rangeWidth / 2) * currentTick);
-  minTick = nearestUsableTick(Math.round(minTick), tickSpacing);
-
-  let maxTick = currentTick + ((rangeWidth / 2) * currentTick);
-  maxTick = nearestUsableTick(Math.round(maxTick), tickSpacing);
-
-  console.log("Ticks: " + minTick + " < " + currentTick + " < " + maxTick);
-
-  return [minTick, maxTick];
-}
-
-function outOfRange(currentTick: number) {
-  return currentTick < dro.minTick || currentTick > dro.maxTick;
-}
-
 // Ethers.js listener:
 // export type Listener = (...args: Array<any>) => void;
 async function onBlock(...args: Array<any>) {
   const state = await getPoolState();
 
-  const oor = outOfRange(state.tick);
+  const oor = dro.outOfRange(state.tick);
 
   const poolEthUsdcForRangeOrder = new Pool(
     dro.usdc,
@@ -176,7 +178,7 @@ async function onBlock(...args: Array<any>) {
     state.tick
   );
 
-  // Log the bloc k number first
+  // Log the block number first
   let logLine = "#" + args;
   let logThisBlock = false;
 
@@ -193,20 +195,17 @@ async function onBlock(...args: Array<any>) {
 
   if (oor) {
     // Tick spacing for the ETH/USDC 0.30% pool is 60.
-    const [minTick, maxTick] = minMaxTicks(state.tick, RANGE_WIDTH, dro.poolImmutables.tickSpacing);
+    const [minTick, maxTick] = dro.newRange(state.tick);
 
     dro.minTick = minTick;
     dro.maxTick = maxTick;
 
-    // tickToPrice() implementation: https://github.com/Uniswap/v3-sdk/blob/6c4242f51a51929b0cd4f4e786ba8a7c8fe68443/src/utils/priceTickConversions.ts#L14
-    const minUsdc = tickToPrice(dro.weth, dro.usdc, minTick).toFixed(2);
-    const maxUsdc = tickToPrice(dro.weth, dro.usdc, maxTick).toFixed(2);
+    // tickToPrice() implementation:
+    //   https://github.com/Uniswap/v3-sdk/blob/6c4242f51a51929b0cd4f4e786ba8a7c8fe68443/src/utils/priceTickConversions.ts#L14
+    // Note that minimum USDC value per ETH corresponds to the maximum tick value and vice versa.
+    const minUsdc = tickToPrice(dro.weth, dro.usdc, maxTick).toFixed(2);
+    const maxUsdc = tickToPrice(dro.weth, dro.usdc, minTick).toFixed(2);
 
-    // console.log("Min USDC: ", tickToPrice(dro.weth, dro.usdc, minTick));
-    // console.log("Max USDC: ", tickToPrice(dro.weth, dro.usdc, maxTick));
-
-    // TODO: These are round the wrong way and way too wide. Half of 1% below 3,000 should be 2,985.
-    // Could the tick spacing of 60 be wrong?
     logLine += " Out of range. New range: " + minUsdc + " USDC - " + maxUsdc + " USDC.";
 
     // Liquidity can be a JSBI, a string or a number.
@@ -231,14 +230,26 @@ async function main() {
   // Get the pool's immutables once only.
   const i = await getPoolImmutables();
 
-  dro = {
-    poolImmutables: i,
-    usdc: new Token(CHAIN_ID, i.token0, 6, "USDC", "USD Coin"),
-    weth: new Token(CHAIN_ID, i.token1, 18, "WETH", "Wrapped Ether"),
-    priceUsdc: "unknown",
-    minTick: 0,
-    maxTick: 0
-  };
+  // From the Uniswap v3 whitepaper:
+  //   "Ticks are all 1.0001 to an integer power, which means each tick is .01% away from the next
+  //    tick."
+  // Note that .01% is one basis point ("bip"), so every tick is a single bip change in price.
+  // But the tick spacing in our pool is 60, so we'd be wise to make our range width a multiple of
+  // that.
+  // Percent   bips (ticks)
+  // -------   ------------
+  //    0.6%             60
+  //    1.2%            120
+  //    1.8%            180
+  //    2.4%            240
+  //    3.0%            300
+  const rangeWidthTicks = 0.006 / 0.0001;
+  console.log("Range width in ticks: " + rangeWidthTicks);
+
+  dro = new DRO(i,
+    new Token(CHAIN_ID, i.token0, 6, "USDC", "USD Coin"),
+    new Token(CHAIN_ID, i.token1, 18, "WETH", "Wrapped Ether"),
+    rangeWidthTicks);
 
   // Get a callback to onBlock() on every new block.
   PROVIDER.on('block', onBlock);
