5afd7117155da275022f1e1f0679bd4b64ec09fa
Eliot Stock <1@eliotstock.com>
2022-03-09 13:09:05 +1300

    Add back-off to proc module.

--- proc/src/index.ts
+++ proc/src/index.ts
@@ -1,8 +1,48 @@
 import * as cp from 'child_process'
 
-console.log(`Running the dro process`)
+const BACKOFF_RETRIES_MAX = 6
+const BACKOFF_DELAY_BASE_SEC = 6
 
-// TODO: Later: npm run prod.
-cp.execSync('npm run n', {'cwd': '../dro'})
+function sleep(seconds: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds * 1000);
+    })
+}
 
-console.log('dro process ended')
+// Do exponential backoff on HTTP error responses from the provider, or indeed anything that can
+// kill the dro process.
+async function main() {
+    console.log(`Running the dro process with back-off`)
+
+    let retries = 0
+
+    do {
+        retries++
+
+        // We do not define the command line for the actual dro process here - that's a script in
+        // the package.json for the dro module next door.
+        // TODO: Later: npm run prod.
+        cp.execSync('npm run n', {'cwd': '../dro'})
+
+        console.log('dro process died')
+
+        if (retries > BACKOFF_RETRIES_MAX) {
+            console.error(`Maximum retries of ${BACKOFF_RETRIES_MAX} exceeded. Fatal.`)
+            process.exit(1)
+        }
+
+        // 6^1: delay for 6 seconds
+        // 6^2: delay for 36 seconds, etc.
+        const delay = Math.pow(BACKOFF_DELAY_BASE_SEC, retries)
+
+        console.log(`Retry #${retries}. Backing off for ${delay} seconds...`)
+        await sleep(delay)
+        console.log(`...done`)
+
+    } while (true)
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
