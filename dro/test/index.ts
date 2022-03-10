a0604d8ad2662449fe0df1c7c0e9c4089ea9768a
Eliot Stock <1@eliotstock.com>
2022-03-11 11:38:16 +1300

    Migrate tests to native bigints.

--- dro/test/index.ts
+++ dro/test/index.ts
@@ -1,6 +1,5 @@
 import { tickToPrice } from '@uniswap/v3-sdk'
 import { expect } from 'chai'
-import { BigNumber } from 'ethers'
 import { rangeOrderPoolTick, updateTick, usdcToken, wethToken } from '../src/uniswap'
 import { EthUsdcWallet } from '../src/wallet'
 
@@ -8,11 +7,11 @@ describe('Wallet token ratio by value', function () {
     it('Should be close to 1.0 for some specific values', async function() {
         // Balances: USDC 1078.48, WETH 0.3926
         // This is USD 1159 worth of WETH, so the ratio should be close to 1.
-        const usdc: BigNumber = BigNumber.from('1078480000')         // USDC 1078.48
-        const weth: BigNumber = BigNumber.from('392600000000000000') // WETH 0.3926
+        const usdc = 1078480000n         // USDC 1078.48
+        const weth = 392600000000000000n // WETH 0.3926
 
         // This is USDC * 10e6, eg. 3_000_000_000 when the price of ETH is USD 3,000.
-        const price: BigNumber = BigNumber.from('3000000000')
+        const price = 3000000000n
 
         const ratio: number = EthUsdcWallet._tokenRatioByValue(usdc, weth, price)
         console.log(`Ratio: ${ratio}`)
@@ -21,11 +20,11 @@ describe('Wallet token ratio by value', function () {
     })
 
     it('Should be 2.0 for some other specific values', async function() {
-        const usdc: BigNumber = BigNumber.from('1000000000')         // USDC 1,000.00
-        const weth: BigNumber = BigNumber.from('500000000000000000') // WETH 0.5
+        const usdc = 1000000000n         // USDC 1,000.00
+        const weth = 500000000000000000n // WETH 0.5
 
         // This is USDC * 10e6, eg. 1_000_000_000 when the price of ETH is USD 1,000.
-        const price: BigNumber = BigNumber.from('1000000000')
+        const price = 1000000000n
 
         const ratio: number = EthUsdcWallet._tokenRatioByValue(usdc, weth, price)
 
@@ -33,11 +32,11 @@ describe('Wallet token ratio by value', function () {
     })
 
     it('Should be 0.5 for some other specific values', async function() {
-        const usdc: BigNumber = BigNumber.from('1000000000')          // USDC 1,000.00
-        const weth: BigNumber = BigNumber.from('2000000000000000000') // WETH 2.0
+        const usdc = 1000000000n          // USDC 1,000.00
+        const weth = 2000000000000000000n // WETH 2.0
 
         // This is USDC * 10e6, eg. 1_000_000_000 when the price of ETH is USD 1,000.
-        const price: BigNumber = BigNumber.from('1000000000')
+        const price: bigint = 1000000000n
 
         const ratio: number = EthUsdcWallet._tokenRatioByValue(usdc, weth, price)
 
