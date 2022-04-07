8b9153c9e88291d723ba163a15bd55afc0c83f02
Eliot Stock <1@eliotstock.com>
2022-04-08 10:26:21 +1200

    Single DRO instance. Command line arg handling extracted. Remove swap monitor.

---
+++ dro/src/command.ts
@@ -0,0 +1,105 @@
+import yargs from 'yargs/yargs'
+import { useConfig, ChainConfig } from './config'
+import { wallet } from './wallet'
+import { updateTick, createPoolOnTestnet } from './uniswap'
+import { DRO } from './dro'
+
+const CHAIN_CONFIG: ChainConfig = useConfig()
+
+// Process command line args using yargs. Pass these to `ts-node ./src/index.ts`.
+export async function handleCommandLineArgs(rangeWidth: number): Promise<[boolean, boolean]> {
+  let noops = false
+
+  const argv = yargs(process.argv.slice(2)).options({
+    n: { type: 'boolean', default: false },
+    balances: { type: 'boolean', default: false },
+    approve: { type: 'boolean', default: false },
+    privateKey: { type: 'boolean', default: false },
+    testnetCreatePool: { type: 'boolean', default: false },
+    wrapEth: { type: 'boolean', default: false },
+  }).parseSync()
+
+  // `--n` means no-op.
+  if (argv.n) {
+    noops = true
+    console.log(`Running in no-op mode. No transactions will be executed.`)
+  }
+
+  // `--balances` means just log our available balances.
+  if (argv.balances) {
+    await updateTick()
+    
+    await wallet.logBalances()
+
+    // To unwrap some WETH, uncomment and run once:
+    // await wallet.unwrapWeth(10_000_000_000_000_000n) // 0.01 WETH
+    // await wallet.logBalances()
+
+    return [noops, true]
+  }
+
+  // `--private-key` means just log the private key for the account.
+  if (argv.privateKey) {
+    console.log(`Private key for Hardhat .env file: ${wallet.privateKey}`)
+
+    return [noops, true]
+  }
+
+  // `--approve` means approve spending of USDC and WETH up to MaxInt.
+  if (argv.approve) {
+    console.log(`Approving spending of USDC and WETH`)
+
+    // await wallet.approveAll(wallet.address)
+
+    // Approve the position manager contract to spend our tokens.
+    await wallet.approveAll(CHAIN_CONFIG.addrPositionManager)
+
+    // Approve the swap routers to spend our tokens.
+    await wallet.approveAll(CHAIN_CONFIG.addrSwapRouter)
+    await wallet.approveAll(CHAIN_CONFIG.addrSwapRouter2)
+
+    return [noops, true]
+  }
+
+  // `--testnet-create-pool` means create a new Uniswap v3 pool with our own USDC token.
+  if (argv.testnetCreatePool) {
+    console.log(`Creating pool on testnet`)
+
+    // Approve the position manager contract to spend our tokens.
+    await wallet.approveAll(CHAIN_CONFIG.addrPositionManager)
+
+    await createPoolOnTestnet()
+
+    return [noops, true]
+  }
+
+  // `--wrap-eth` means wrap some ETH to WETH.
+  if (argv.wrapEth) {
+    // Required 1_250_000_000_000_000_000
+    // Got      1_186_361_607_590_516_298
+    await wallet.wrapEth('0.5')
+
+    return [noops, true]
+  }
+
+  // `--r` means remove liquidity from the dro and exit.
+  if (argv.r) {
+    console.log(`Removing liquidity only.`)
+
+    await updateTick()
+
+    const dro: DRO = new DRO(rangeWidth, false)
+    await dro.init()
+
+    if (dro.inPosition()) {
+      await dro.removeLiquidity()
+    }
+    else {
+      console.log(`dro with width ${rangeWidth} wasn't in position. No liquidity to remove.`)
+    }
+
+    return [noops, true]
+  }
+
+  return [noops, false]
+}
--- dro/src/index.ts
+++ dro/src/index.ts
@@ -1,11 +1,10 @@
 import { config } from 'dotenv'
 import moment from 'moment'
-import yargs from 'yargs/yargs'
 import { useConfig, ChainConfig } from './config'
-import { wallet, updateGasPrice, gasPriceFormatted } from './wallet'
-import { updateTick, priceFormatted, createPoolOnTestnet } from './uniswap'
+import { updateGasPrice, gasPriceFormatted } from './wallet'
+import { handleCommandLineArgs } from './command'
+import { updateTick, priceFormatted } from './uniswap'
 import { DRO } from './dro'
-import { monitor } from './swap-monitor'
 
 // Read our .env file
 config()
@@ -21,19 +20,15 @@ const CHAIN_CONFIG: ChainConfig = useConfig()
 // Note that .01% is one basis point ("bip"), so every tick is a single bip change in price.
 // Tick spacing is 10 for the 0.05% fee tier pool, 60 for the 0.30% fee tier pool.
 // Range width should be a multiple of the tick spacing, with a minimum of double the tick spacing.
-if (process.env.RANGE_WIDTHS == undefined) throw 'No RANGE_WIDTHS list in .env file.'
+if (process.env.RANGE_WIDTH == undefined) throw 'No RANGE_WIDTH in .env file.'
 
-const rangeWidths: number[] = process.env.RANGE_WIDTHS?.split(' ').map(Number)
-
-// Set of DRO instances on which we are forward testing. In production this should only have one
-// member.
-let dros: DRO[] = []
+const rangeWidth: number = Number(process.env.RANGE_WIDTH)
 
 // Price of WETH in USDC terms in the range order pool, formatted to two decimal places.
 let price: string
 
-// When invoked with the -n command line arg, don't execute any transactions.
-let noops: boolean = false
+// Single DRO instance.
+let dro: DRO
 
 // Ethers.js listener:
 // export type Listener = (...args: Array<any>) => void
@@ -50,131 +45,24 @@ async function onBlock(...args: Array<any>) {
     console.log(`${moment().format("MM-DD-HH:mm:ss")} #${args} ${gasPriceFormatted()} \
 ${priceFormatted()} USDC`)
 
-    for (const dro of dros) {
-      await dro.onPriceChanged()
-    }
+    dro.onPriceChanged()
   }
 
   price = priceFormatted()
 
-  // Pass the onBlock() call through to each DRO instance, which will figure out whether it needs
+  // Pass the onBlock() call through to the DRO instance, which will figure out whether it needs
   // to re-range and execute transactions if so.
-  for (const dro of dros) {
-    await dro.onBlock()
-  }
+  await dro.onBlock()
 }
 
 async function main() {
   console.log(`Chain: ${CHAIN_CONFIG.name}`)
 
-  // Process command line args using yargs. Pass these to `ts-node ./src/index.ts`
-  const argv = yargs(process.argv.slice(2)).options({
-    n: { type: 'boolean', default: false },
-    balances: { type: 'boolean', default: false },
-    monitor: { type: 'boolean', default: false },
-    approve: { type: 'boolean', default: false },
-    privateKey: { type: 'boolean', default: false },
-    testnetCreatePool: { type: 'boolean', default: false },
-    wrapEth: { type: 'boolean', default: false },
-  }).parseSync()
+  // When invoked with the -n command line arg, don't execute any transactions.
+  const [noops, exitNow] = await handleCommandLineArgs(rangeWidth)
 
-  // `--n` means no-op.
-  if (argv.n) {
-    noops = true
-    console.log(`Running in no-op mode. No transactions will be executed.`)
-  }
-
-  // `--balances` means just log our available balances.
-  if (argv.balances) {
-    await updateTick()
-    
-    await wallet.logBalances()
-
-    // To unwrap some WETH, uncomment and run once:
-    // await wallet.unwrapWeth(10_000_000_000_000_000n) // 0.01 WETH
-    // await wallet.logBalances()
-
-    return
-  }
-
-  // `--monitor` means just log the prices in the pool.
-  if (argv.monitor) {
-    console.log(`Monitoring swaps in the pool`)
-    
-    try {
-      monitor(CHAIN_CONFIG)
-    }
-    catch(e) {
-      // Probably network error
-      console.error(e)
-    }
-
-    return
-  }
-
-  // `--private-key` means just log the private key for the account.
-  if (argv.privateKey) {
-    console.log(`Private key for Hardhat .env file: ${wallet.privateKey}`)
-
-    return
-  }
-
-  // `--approve` means approve spending of USDC and WETH up to MaxInt.
-  if (argv.approve) {
-    console.log(`Approving spending of USDC and WETH`)
-
-    // await wallet.approveAll(wallet.address)
-
-    // Approve the position manager contract to spend our tokens.
-    await wallet.approveAll(CHAIN_CONFIG.addrPositionManager)
-
-    // Approve the swap routers to spend our tokens.
-    await wallet.approveAll(CHAIN_CONFIG.addrSwapRouter)
-    await wallet.approveAll(CHAIN_CONFIG.addrSwapRouter2)
-
-    return
-  }
-
-  // `--testnet-create-pool` means create a new Uniswap v3 pool with our own USDC token.
-  if (argv.testnetCreatePool) {
-    console.log(`Creating pool on testnet`)
-
-    // Approve the position manager contract to spend our tokens.
-    await wallet.approveAll(CHAIN_CONFIG.addrPositionManager)
-
-    await createPoolOnTestnet()
-
-    return
-  }
-
-  // `--wrap-eth` means wrap some ETH to WETH.
-  if (argv.wrapEth) {
-    // Required 1_250_000_000_000_000_000
-    // Got      1_186_361_607_590_516_298
-    await wallet.wrapEth('0.5')
-
-    return
-  }
-
-  // `--r` means remove liquidity from all dros and exit.
-  if (argv.r) {
-    console.log(`Removing liquidity only.`)
-
-    await updateTick()
-
-    for (const width of rangeWidths) {
-      const dro: DRO = new DRO(width, false)
-      await dro.init()
-
-      if (dro.inPosition()) {
-        await dro.removeLiquidity()
-      }
-      else {
-        console.log(`dro with width ${width} wasn't in position. No liquidity to remove.`)
-      }
-    }
-
-    return
+  if (exitNow) {
+    process.exit(0)
   }
 
   // The absence of a try/catch block below is deliberate. The execution of main() already has one.
@@ -184,12 +72,8 @@ async function main() {
   // We must have the price in the range order pool before we can establish a range.
   await updateTick()
 
-  for (const width of rangeWidths) {
-    const dro: DRO = new DRO(width, noops)
-    await dro.init()
-
-    dros.push(dro)
-  }
+  dro = new DRO(rangeWidth, noops)
+  await dro.init()
 
   // Get a callback to onBlock() on every new block.
   CHAIN_CONFIG.provider().on('block', onBlock)
